d39bf573aff25ec36b7b14817c90d10cf136f030
sawiw <96121984+sawiw@users.noreply.github.com>
2022-07-26 01:12:11 +0200

    js dans app js  + corriger wp

--- assets/app.js
+++ assets/app.js
@@ -11,3 +11,24 @@ import './styles/app.scss';
 // start the Stimulus application
 import './bootstrap';
 
+//add wp
+
+const addFormToCollection = (e) => {
+    const collectionHolder = document.querySelector('.' + e.currentTarget.dataset.collectionHolderClass);
+
+    const item = document.createElement('li');
+
+    item.innerHTML = collectionHolder
+        .dataset
+        .prototype
+        .replace(/__name__/g,
+            collectionHolder.dataset.index
+        );
+
+    collectionHolder.appendChild(item);
+    collectionHolder.dataset.index++;
+}
+//onClick
+document.querySelectorAll('.add_item_link').forEach(btn => {
+    btn.addEventListener('click', addFormToCollection)
+});
