560099b770a730765254f572d2728eb5d4c81893
sawiw <sammashd@gmail.com>
2022-07-27 23:45:57 +0200

    dockerizer avec https://github.com/dunglas/symfony-docker/

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ Encore
      * ENTRY CONFIG
      *
      * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
     .addEntry('project', './assets/project.js')
@@ -51,7 +51,6 @@ Encore
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning()
     .enableVersioning(Encore.isProduction())
 
     .configureBabel((config) => {
@@ -80,7 +79,7 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    // .autoProvidejQuery()
+    //.autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
